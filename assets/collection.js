91d9e96266d976131f01e94b1754d46b942498da
Julien A <128476445+Arnould-Julien@users.noreply.github.com>
2023-05-12 17:41:00 +0200

    Add files via upload
    first commit (ajout collection)

---
+++ assets/collection.js
@@ -0,0 +1,272 @@
+const collection = [
+    {
+        name: "Minecraft",
+        editors: [
+            "Mojang Studios", 
+            "Xbox Game Studios", 
+            "Sony Interactive Entertainment"
+        ],
+        releaseYear: 2011,
+        cover: "url('./assets/img/gameCovers/minecraft.jpg')",
+        types: [
+            "Sandbox", 
+            "Survival", 
+            "Build"
+        ],
+        modes: [
+            "solo",
+            "multiplayer"
+        ],
+        sales: 238000000,
+        platforms: [
+            "Microsoft",
+            "Windows", 
+            "MacOS", 
+            "Linux", 
+            "Xbox 360", 
+            "Xbox One", 
+            "Xbox Series", 
+            "PlayStation 3", 
+            "PlayStation 4", 
+            "PlayStation 5", 
+            "PlayStation Vita", 
+            "iOS", 
+            "iPad", 
+            "Apple TV", 
+            "Android", 
+            "Windows Phone", 
+            "Kindle Fire", 
+            "New Nintendo 3DS", 
+            "Wii U", 
+            "Nintendo Switch"
+        ]
+    },
+    {
+        name: "Grand Theft Auto V",
+        editors: [
+            "Rockstar Games",
+            "Take-Two Interactive", 
+            "Softclub"
+        ],
+        releaseYear: 2013,
+        cover: "url('./assets/img/gameCovers/gtaV.jpg')",
+        types: [
+            "Action", 
+            "Adventure", 
+            "Shooter"
+        ],
+        modes: [
+            "solo",
+            "multiplayer"
+        ],
+        sales: 175000000,
+        platforms: [
+            "PlayStation 3", 
+            "Xbox 360", 
+            "PlayStation 4", 
+            "Xbox One", 
+            "Microsoft Windows", 
+            "PlayStation 5", 
+            "Xbox Series"
+        ]
+    },
+    {
+        name: "Red Dead Redemption II",
+        editors: [
+            "Rockstar Games"
+        ],
+        releaseYear: 2018,
+        cover: "url('./assets/img/gameCovers/rdr2.jpg')",
+        types: [
+            "Action", 
+            "Adventure", 
+            "Shooter"
+        ],
+        modes: [
+            "solo",
+            "multiplayer"
+        ],
+        sales: 50000000,
+        platforms: [
+            "PlayStation 3", 
+            "Xbox 360", 
+            "PlayStation 4", 
+            "Xbox One", 
+            "Microsoft Windows", 
+            "PlayStation 5", 
+            "Xbox Series"
+        ]
+    },
+    {
+        name: "Terraria",
+        editors: [
+            "Re-Logic", 
+            "505 Games",
+            "Spike Chunsoft" 
+        ],
+        releaseYear: 2011,
+        cover: "url('./assets/img/gameCovers/terraria.avif')",
+        types: [
+            "Sandbox", 
+            "Adventure"
+        ],
+        modes: [
+            "solo",
+            "multiplayer"
+        ],
+        sales: 44050000,
+        platforms: [
+            "Microsoft Windows", 
+            "OS X", 
+            "Linux, Apple iOS", 
+            "Android", 
+            "Windows Phone", 
+            "PlayStation 4", 
+            "PlayStation 3", 
+            "PlayStation Vita", 
+            "Xbox One", 
+            "Xbox 360", 
+            "Wii U", 
+            "Nintendo 3DS", 
+            "Nintendo Switch"
+        ]
+    },
+    {
+        name: "Super Mario Bros",
+        editors: [
+            "Nintendo"
+        ],
+        releaseYear: 1985,
+        cover: "url('./assets/img/gameCovers/supermariobros.jpg')",
+        types: [
+            "Platforms"
+        ],
+        modes: [
+            "solo",
+            "two players"
+        ],
+        sales: 43017000,
+        platforms: [
+            "Nintendo Entertainment System", 
+            "Game Boy Advance", 
+            "Wii"
+        ]
+    },
+    {
+        name: "The Witcher 3 : Wild Hunt",
+        editors: [
+            "CD Projekt"
+        ],
+        releaseYear: 2015,
+        cover: "url('./assets/img/gameCovers/the-witcher-3.jpg')",
+        types: [
+            "RPG"
+        ],
+        modes: [
+            "solo"
+        ],
+        sales: 40000000,
+        platforms: [
+            "Microsoft Windows", 
+            "Xbox One", 
+            "PlayStation 4", 
+            "Nintendo Switch", 
+            "PlayStation 5", 
+            "Xbox Series"
+        ]
+    },
+    {
+        name: "Zelda The Breath of the Wild",
+        editors: [
+            "Nintendo"
+        ],
+        releaseYear: 2017,
+        cover: "url('./assets/img/gameCovers/zeldaofthewild.jpg')",
+        types: [
+            "Action"
+        ],
+        modes: [
+            "solo"
+        ],
+        sales: 30070000,
+        platforms: [
+            "Nintendo Switch", 
+            "Wii U"
+        ]
+    },
+    {
+        name: "Diablo III",
+        editors: [
+            "Blizzard Entertainment"
+        ],
+        releaseYear: 2012,
+        cover: "url('./assets/img/gameCovers/diablo-3.jpg')",
+        types: [
+            "Hack'n'slash"
+        ],
+        modes: [
+            "solo",
+            "multiplayer"
+        ],
+        sales: 30000000,
+        platforms: [
+            "Windows", 
+            "Mac OS X", 
+            "PS3", 
+            "PS4", 
+            "Xbox 360", 
+            "Xbox One", 
+            "Nintendo Switch"
+        ]
+    },
+    {
+        name: "The Elder Scrolls V : Skyrim",
+        editors: [
+            "Bethesda Softworks"
+        ],
+        releaseYear: 2011,
+        cover: "url('./assets/img/gameCovers/elder-scroll-5.jpg')",
+        types: [
+            "RPG"
+        ],
+        modes: [
+            "solo"
+        ],
+        sales: 30000000,
+        platforms: [
+            "Microsoft Windows", 
+            "PlayStation 3", 
+            "Xbox 360", 
+            "PlayStation 4", 
+            "Xbox One", 
+            "Nintendo Switch"
+        ]
+    },
+    {
+        name: "Border land 2",
+        editors: [
+            "Bethesda Softworks"
+        ],
+        releaseYear: 2011,
+        cover: "url('./assets/img/gameCovers/borderlands2.jpg')",
+        types: [
+            "Action",
+            "Shooter",
+            "Hack'n'slash"
+        ],
+        modes: [
+            "solo",
+            "co-op",
+            "Four players"
+        ],
+        sales: 27000000,
+        platforms: [
+            "Microsoft Windows", 
+            "PlayStation 3", 
+            "Xbox 360", 
+            "PlayStation 4", 
+            "Xbox One", 
+            "Nintendo Switch"
+        ]
+    }
+]
